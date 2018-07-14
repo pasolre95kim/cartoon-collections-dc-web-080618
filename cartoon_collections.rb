4e05b9e2d7b881e15df386d5faac879484ebd03a
Michelle Kim <pasolre95kim@gmail.com>
2018-07-14 19:46:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,12 +7,13 @@ end
 
 def summon_captain_planet(array)
   array.collect {|call| call + "!"}
-  # Your code here
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.length > 4
+end 
 
 def find_the_cheese# code an argument here
   # the array below is here to help
